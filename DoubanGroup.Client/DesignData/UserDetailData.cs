8d1b04178e246db5e614f0e5171d8d0f9f330dac
scheshan <5373827@qq.com>
2015-10-31 21:31:05 +0800

    add user detail page

---
+++ DoubanGroup.Client/DesignData/UserDetailData.cs
@@ -0,0 +1,19 @@
+﻿using DoubanGroup.Core.Api.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubanGroup.Client.DesignData
+{
+    public class UserDetailData
+    {
+        public User User { get; private set; }
+
+        public UserDetailData()
+        {
+
+        }
+    }
+}
